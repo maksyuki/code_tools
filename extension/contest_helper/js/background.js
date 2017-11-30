5c188a6548c1ce957e0caf6895173a7cded0c599
maksyuki <myc20404401@sina.com>
2017-11-30 20:37:50 +0800

    Add contest_helper extension

---
+++ extension/contest_helper/js/background.js
@@ -0,0 +1,37 @@
+chrome.contextMenus.create({
+    'type':'normal',
+    'title':'copy input data to ClipBoard',
+    'contexts':['selection'],
+    'id':'cn',
+    'onclick':copydata
+});
+
+function translate(info, tab){
+    var url = 'http://translate.google.com.hk/#auto/zh-CN/'+info.selectionText ;
+    window.open(url, '_blank');
+}
+
+function copydata(info, tab){
+    var data = info.selectionText;
+    // alert(data);//this is available options to show data.
+    var transfer = document.getElementById('CopyTransfer');
+        if (!transfer) {
+            transfer = document.createElement('textarea');
+            transfer.id = 'CopyTransfer';
+            transfer.style.position = 'absolute';
+            transfer.style.left = '-9999px';
+            transfer.style.top = '-9999px';
+            document.body.appendChild(transfer);
+        }
+        transfer.value = data;
+        transfer.focus();
+        transfer.select();
+        var is_copy = document.execCommand('Copy', false, null);
+        alert(is_copy);
+}
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+    chrome.contextMenus.update('cn',{
+        'title':'使用Google翻译“'+message+'”'
+    });
+});
